2f2352c4f694f384f17ae263465783e5fd9640cc
Maria Soria <maria.it3@gmail.com>
2022-12-19 19:07:32 +0000

    Add implementation to first day of advent of code 2022, part 2

--- src/firstDecember/CalculateCalories.ts
+++ src/firstDecember/CalculateCalories.ts
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs';
 const sum = (meal1: string, meal2: string): string =>
   (parseInt(meal1) + parseInt(meal2)).toString();
 
-const calculateElfTotalCalories = (elfLoad) =>
+const calculateElfTotalCalories = (elfLoad): string =>
   elfLoad.split(/\n/).reduce(sum);
 
 const getHighestCaloriesAmount = (elf1, elf2) => {
@@ -13,8 +13,46 @@ const getHighestCaloriesAmount = (elf1, elf2) => {
   return parseInt(totalCaloriesElf1) > parseInt(totalCaloriesElf2) ? totalCaloriesElf1 : totalCaloriesElf2;
 };
 
-export const calculateCalories = (filename: string): string => {
+const getFileContentFormatted = (filename: string) => {
   let allContents = readFileSync(filename, 'utf-8');
-  allContents = allContents.slice(0, allContents.length - 1);
+  return allContents.slice(0, allContents.length - 1);
+};
+
+export const calculateCalories = (filename: string): string => {
+  const allContents = getFileContentFormatted(filename);
   return allContents.split(/\n\n/).reduce(getHighestCaloriesAmount);
+};
+
+const calculateTotalCaloriesByElf = (fileContent: string) => {
+  let totalCaloriesByElf: number[] = [];
+  fileContent.split(/\n\n/).forEach(elf => {
+    const totalElf: number = parseInt(calculateElfTotalCalories(elf));
+    totalCaloriesByElf.push(totalElf);
+  });
+  return totalCaloriesByElf;
+};
+
+const ascendantSort = (elf1, elf2) => {
+  if (elf1 < elf2) {
+    return 1;
+  }
+
+  if (elf1 > elf2) {
+    return -1;
+  }
+
+  return 0;
+};
+
+const getTotalOfTopThree = (totalCaloriesByElfSorted: number[]): string => {
+  return (totalCaloriesByElfSorted[0]
+    + totalCaloriesByElfSorted[1]
+    + totalCaloriesByElfSorted[2]).toString();
+};
+
+export const calculateTotalTopThreeCalories = (filename: string): string => {
+  const fileContent = getFileContentFormatted(filename);
+  let totalCaloriesByElf = calculateTotalCaloriesByElf(fileContent);
+  const totalCaloriesByElfSorted = totalCaloriesByElf.sort(ascendantSort);
+  return getTotalOfTopThree(totalCaloriesByElfSorted);
 };
--- src/firstDecember/__test__/CalculateCalories.test.ts
+++ src/firstDecember/__test__/CalculateCalories.test.ts
@@ -1,7 +1,13 @@
-import { calculateCalories } from '../CalculateCalories';
+import { calculateCalories, calculateTotalTopThreeCalories } from '../CalculateCalories';
 
 describe('calculate calories', () => {
   it('should calculate the highest amount of calories', function () {
     expect(calculateCalories('src/firstDecember/calories.txt')).toEqual('71506');
+  });
+});
+
+describe('Day 1 part 2', () => {
+  it('should calculate the top three highest amount of calories', function () {
+    expect(calculateTotalTopThreeCalories('src/firstDecember/calories.txt')).toEqual('209603');
   });
 });
